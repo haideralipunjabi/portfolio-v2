b94c9a9e524ee6c26763d19f6468682aaa1abf3b
Haider Ali Punjabi <haiderali@cyberservices.com>
2020-07-30 16:20:54 +0530

    Added FOSS

--- .travis/build.py
+++ .travis/build.py
@@ -7,23 +7,55 @@ import os
 from os import listdir
 from os.path import isfile, join, splitext
 import requests
-
-
+import feedparser
+from functools import reduce
 templateLoader = jinja2.FileSystemLoader(searchpath="./")
 templateEnv = jinja2.Environment(loader=templateLoader)
+
+BLOG_RSS_URL = "https://blog.haideralipunjabi.com/index.xml"
+GITHUB_API_URL = "https://api.github.com/users/%s/repos?per_page=100&page=%s"
+def get_blog_posts(num):
+    feed = feedparser.parse(BLOG_RSS_URL)
+    return {"blog_posts":feed['entries'][:num]}
+
+def get_github_data(username):
+    repos = []
+    page = 1
+    while True:
+        data = requests.get(GITHUB_API_URL%(username,page)).json()
+        repos.extend(data)
+        if len(data) < 100:
+            break
+        page += 1
+    source_repos_len = len(list(filter(lambda repo: repo["fork"]==False,repos)))
+    stargazers = reduce(lambda a,b: a+b["stargazers_count"],repos,0)
+    forks = reduce(lambda a,b: a+b["forks"],repos,0)
+    most_popular = list(sorted(repos, key=lambda repo: repo['stargazers_count'],reverse=True))[:3]
+    return {
+        "github":{
+            "repo_count": source_repos_len,
+            "stargazers": stargazers,
+            "forks": forks,
+            "most_popular": most_popular
+        }
+    }
 
 templates = [
     {
         "input": "index.html",
-        "data": ["backpack.json","settings.json","projects.json","timeline.json"],
+        "data_files": ["backpack.json","settings.json","projects.json","timeline.json"],
+        "data":[get_blog_posts(5),get_github_data("haideralipunjabi")],
         "output": "index.html"
     },
     {
         "input": "colors.css",
-        "data": ["settings.json"],
+        "data_files": ["settings.json"],
+        "data":[],
         "output": "assets/css/colors.css"
     }
 ]
+
+
 
 def gen_favicons():
     icon_svg = "assets/media/icon.svg"
@@ -40,11 +72,15 @@ def gen_templates():
     for template_data in templates:
         TEMPLATE_FILE = "templates/"+template_data["input"]
         data = {}
-        for file in template_data["data"]:
+        for file in template_data["data_files"]:
             data_file = json.load(open("data/"+file,"r"))
             data.update(data_file)
+        for d in template_data["data"]:
+            data.update(d)
         template = templateEnv.get_template(TEMPLATE_FILE)
         print(template.render(**data),file=open(template_data["output"],"w"))
+
+
 
 def gen_sitemap():
     sitemap_file = open("sitemap.xml","w")
