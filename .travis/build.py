905036f3b839bef0b34ad3c2cb7d17c232408df3
Haider Ali Punjabi <haiderali@cyberservices.com>
2020-08-14 16:28:24 +0530

    [ci-skip] Added Press Section

--- .travis/build.py
+++ .travis/build.py
@@ -11,19 +11,24 @@ from PIL import Image
 templateLoader = jinja2.FileSystemLoader(searchpath="./")
 templateEnv = jinja2.Environment(loader=templateLoader,trim_blocks=True)
 
+NO_DOWNLOAD = False
+
 BLOG_API_URL = "https://blog.haideralipunjabi.com/index.json"
 GITHUB_API_URL = "https://api.github.com/users/%s/repos?per_page=100&page=%s"
 FOSS_CONTRIBUTIONS_DATA = "data/foss-contributions.json"
 BLOG_IMAGE_SIZE = 512,512
+
 def get_blog_posts(num):
     data = requests.get(BLOG_API_URL).json()
-    os.system("rm -rf assets/img/blogs")
-    os.system("mkdir assets/img/blogs")
+    if not NO_DOWNLOAD:
+        os.system("rm -rf assets/img/blogs")
+        os.system("mkdir assets/img/blogs")
     for post in data[:num]:
         filein = f'assets/img/blogs/{post["data"]["image"].split("/")[-1]}'
         fileout1 = filein.rsplit(".",1)[0] + ".webp"
         fileout2 = filein.rsplit(".",1)[0] + ".jpg"
-        wget.download(post["data"]["image"],out=filein)
+        if not NO_DOWNLOAD:
+            wget.download(post["data"]["image"],out=filein)
         post["data"]["image"] = [fileout1,fileout2]
         im = Image.open(filein)
         im.thumbnail(BLOG_IMAGE_SIZE,Image.ANTIALIAS)
@@ -31,6 +36,34 @@ def get_blog_posts(num):
         im.save(fileout2)
         
     return {"blog_posts":data[:num]}
+
+def get_press_posts(num):
+    data = json.load(open("data/press.json","r"))
+    if not NO_DOWNLOAD:
+        os.system("rm -rf assets/img/press")
+        os.system("mkdir assets/img/press")
+    for i,post in enumerate(data["press"]):
+        if "title" not in post.keys() or post["title"] == "":
+            r = requests.get(f"https://api.urlmeta.org/?url={post['link']}", headers={
+                "Authorization": f"Basic {os.getenv('URLMETA_API')}"
+            }).json()
+            post["title"] = r["meta"]["title"]
+            post["description"]=r["meta"]["description"]
+            post["image_src"] = r["meta"]["image"]
+            post["site"] = r["meta"]["site"]["name"]
+        if num > i:    
+            filein = f'assets/img/press/{i}.{post["image_src"].split(".")[-1]}'
+            fileout1 = filein.rsplit(".",1)[0] + ".webp"
+            fileout2 = filein.rsplit(".",1)[0] + ".jpg"
+            if not NO_DOWNLOAD:
+                wget.download(post["image_src"],out=filein)
+            im = Image.open(filein)
+            im.thumbnail(BLOG_IMAGE_SIZE,Image.ANTIALIAS)
+            im.save(fileout1)
+            im.save(fileout2)
+            post["images"] = [fileout1,fileout2]
+    json.dump(data,fp=open("data/press.json","w"))
+    return {"press":data["press"]}
 
 def get_github_data(username,num):
     repos = []
@@ -67,7 +100,7 @@ templates = [
     {
         "input": "index.html",
         "data_files": ["backpack.json","settings.json","projects.json","timeline.json"],
-        "data":[get_blog_posts(4),get_github_data("haideralipunjabi",4),get_foss_contributions()],
+        "data":[get_blog_posts(4),get_github_data("haideralipunjabi",4),get_foss_contributions(),get_press_posts(4)],
         "output": "index.html"
     },
     {
--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,9 @@ const navbar = document.getElementById("nav-bar");
 const sticky = navbar.offsetTop;
 const timelineButton = document.querySelector(".timeline-button");
 
-const errorSpan = document.createElement('span');
-errorSpan.textContent = "This field is required!"
-errorSpan.classList.add('error')
+const errorSpan = document.createElement("span");
+errorSpan.textContent = "This field is required!";
+errorSpan.classList.add("error");
 
 window.onscroll = function shiftNav() {
   if (window.pageYOffset > sticky) {
@@ -39,45 +39,67 @@ function closeMenu() {
   document.querySelector(".menu-links").classList.remove("fade-down");
 }
 
-
-timelineButton.addEventListener('click',()=>{
-    document.querySelector(".timeline").classList.toggle("timeline-collapsed");
-  })
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function () {
-    navigator.serviceWorker.register('/sw.js').then(function (registration) {
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function (err) {
-
-      console.log('ServiceWorker registration failed: ', err);
-    });
+timelineButton.addEventListener("click", () => {
+  document.querySelector(".timeline").classList.toggle("timeline-collapsed");
+});
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker.register("/sw.js").then(
+      function (registration) {
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      },
+      function (err) {
+        console.log("ServiceWorker registration failed: ", err);
+      }
+    );
   });
 }
-window.addEventListener('beforeinstallprompt', (e) => {
+window.addEventListener("beforeinstallprompt", (e) => {
   // Prevent the mini-infobar from appearing on mobile
   e.preventDefault();
   // Stash the event so it can be triggered later.
-
 });
 
-document.getElementById("contact-form").addEventListener('submit',e=>{
+document.getElementById("contact-form").addEventListener("submit", (e) => {
   e.preventDefault();
   let name = document.getElementById("from_name").value;
   let email = document.getElementById("email").value;
   let msg = document.getElementById("message").value;
   const message = document.getElementById("msg");
-  let data = encodeURI(`Name: ${name}\nEmail: ${email}\nMessage: ${msg}`)
-  
-  fetch(`/contact/${data}`, {
-    'method': 'GET'
-  }).then(r=>{
-    if(r.status === 200){
-      message.style.display = "block";
-        message.textContent = "Successfully send message!";
+  let data = encodeURI(`Name: ${name}\nEmail: ${email}\nMessage: ${msg}`);
 
-        setTimeout(() => {
-          message.style.display = "none";
-        }, 10000);
+  fetch(`/contact/${data}`, {
+    method: "GET",
+  }).then((r) => {
+    if (r.status === 200) {
+      message.style.display = "block";
+      message.textContent = "Successfully send message!";
+
+      setTimeout(() => {
+        message.style.display = "none";
+      }, 10000);
     }
-  })
-})
+  });
+});
+
+function closeModal(e) {
+  modal = document.querySelector(e);
+  modal.classList.remove("open");
+}
+function openModal(e) {
+  modal = document.querySelector(e);
+  modal.classList.add("open");
+}
+window.onclick = (event) => {
+  if ([...document.querySelectorAll(".modal")].includes(event.target)) {
+    event.target.classList.remove("open");
+  }
+};
+// document.querySelectorAll(".modal").forEach(el=>{
+//   el.addEventListener('click',()=>{
+//     el.classList.remove('open');
+//   })
+// })
